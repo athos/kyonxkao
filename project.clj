21909d6c4f7b5e8a1556549bcbb21841210e927e
OHTA Shogo <athos0220@gmail.com>
2014-04-01 22:30:29 +0900

    first commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject kyonkao "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.5-SNAPSHOT"]]}}
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
