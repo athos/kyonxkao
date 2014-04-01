ec542c2d7b017570b78664170c644ed602aa5b28
OHTA Shogo <athos0220@gmail.com>
2014-04-02 06:25:25 +0900

    implement the parser skeleton

--- src/kyonkao/core.clj
+++ src/kyonkao/core.clj
@@ -1,6 +1,25 @@
 (ns kyonkao.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parse-title [lines]
+  )
+
+(defn parse-characters [lines]
+  )
+
+(defn parse-line [lines]
+  )
+
+(defn parse-scene [lines]
+  )
+
+(defn parse-scenes [lines]
+  )
+
+(defn parse-acts [lines]
+  )
+
+(defn parse-program [lines]
+  (let [[_ lines] (parse-title lines)
+        [characters lines] (parse-characters lines)
+        [acts lines] (parse-acts lines)]
+    [characters acts]))
