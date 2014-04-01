ab705467d85b5a81fea310a164ec6a9d72d64998
OHTA Shogo <athos0220@gmail.com>
2014-04-02 06:23:11 +0900

    add some dependencies

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.5-SNAPSHOT"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.5-SNAPSHOT"]]
+                   :source-paths ["src" "dev"]}}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
